655202585fb77f57ef652840d82339190e86d376
Fatih Arapoglu <f3tt4n@gmail.com>
2022-12-15 02:46:14 +0300

    add some local storage commits

--- src/localstorage.js
+++ src/localstorage.js
@@ -2,7 +2,7 @@ import { Wrap, Project, Task } from "./classes";
 import { DOM } from "./DOM";
 
 class Storage {
-    static setProjectsToStorage(projects) {
+    static setProjectsToStorage(projects) { // basicly saves "wrapper" everytime DOM renders
         localStorage.setItem("projects", JSON.stringify(projects));
     }
     static getProjectsFromStorage() {
@@ -14,12 +14,12 @@ class Storage {
         let projects = DOM.wrapper.getProjectList();
         this.setProjectsToStorage(projects);
     }
-    static loadFromStorage() {
+    static loadFromStorage() { // the saved storage is an object which is useless if it's not a class prototype of the related tasks and projects
         let newWrapper = new Wrap();
         let retrievedProjects = this.getProjectsFromStorage();
         if (retrievedProjects === null) {
             DOM.pushDefault();
-        } else {
+        } else { // so we have to assign classes to all stored objects to use related methods
             newWrapper.setProjectList(retrievedProjects);
             newWrapper.setProjectList(
                 newWrapper.getProjectList().map(
@@ -37,17 +37,17 @@ class Storage {
             DOM.activeProject = newWrapper.getProjectList().find(project => project.getName() == " myTask");
         }
     }
-    static clearLocalStorage() {
+    static clearLocalStorage() { // for clearing the local storage
         localStorage.clear();
         DOM.snackbar("Local storage cleared, refresh to see the default page.");
     }
-    static setTheme(mode) {
+    static setTheme(mode) { // set color theme from DOM module
         localStorage.setItem("theme", mode);
     }
     static getTheme() {
         if (localStorage.getItem("theme") === null || localStorage.getItem("theme") == "dark") {
-            return;
-        } else {
+            return; // if there is no storaged or the storaged theme is dark, nothing to do
+        } else { // else change the theme from DOM module
             DOM.themeChanger();
         }
     }
